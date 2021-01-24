121e5981930a3e9ba9afdadf645f2774d1ebbe34
liaoxiuli <liaoxiuli@weiling.cn>
2021-01-24 18:13:12 +0800

    init

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ class Promise {
     try {
       this.value = null;
       this.status = Promise.PENDING;
+      this.callbacks = [];
       excutor(this.resolve.bind(this), this.reject.bind(this));
     } catch (error) {
       this.reject.bind(this, error);
@@ -15,28 +16,61 @@ class Promise {
     if (this.status === Promise.PENDING) {
       this.status = Promise.FULLFILLED;
       this.value = value;
+      this.callbacks.map((callback) => {
+        callback.onFullfilled(this.value);
+      });
     }
   }
   reject(reason) {
     if (this.status === Promise.PENDING) {
       this.status = Promise.REJECTED;
       this.value = reason;
+      this.callbacks.map((callback) => {
+        callback.onRejected(this.value);
+      });
     }
   }
   then(onFullfilled, onRejected) {
-    if(typeof onFullfilled !== "function"){
+    if (typeof onFullfilled !== "function") {
       onFullfilled = () => {};
     }
 
-    if(typeof onRejected !== "function"){
+    if (typeof onRejected !== "function") {
       onRejected = () => {};
     }
-    
-    if (this.status === Promise.FULLFILLED) {
-      onFullfilled(this.value);
-    }
-    if (this.status === Promise.REJECTED) {
-      onRejected(this.value);
-    }
+    setTimeout(() => {
+      if (this.status === Promise.PENDING) {
+        this.callbacks.push({
+          onFullfilled: (value) => {
+            try {
+              onFullfilled(value);
+            } catch (error) {
+              onRejected(error);
+            }
+          },
+          onRejected: (reason) => {
+            try {
+              onRejected(reason);
+            } catch (error) {
+              onRejected(error);
+            }
+          },
+        });
+      }
+      if (this.status === Promise.FULLFILLED) {
+        try {
+          onFullfilled(this.value);
+        } catch (error) {
+          onRejected(error);
+        }
+      }
+      if (this.status === Promise.REJECTED) {
+        try {
+          onFullfilled(this.value);
+        } catch (error) {
+          onRejected(error);
+        }
+      }
+    });
   }
 }
